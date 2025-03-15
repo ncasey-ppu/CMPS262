b37baff2ccede2106ddb20879a6e649981a59f97
ncasey-ppu <ncasey@pointpark.edu>
2025-03-15 13:03:30 -0400

    removed contact html
    corrected errors with contact page on app.js and removed contact html

--- Module2/app.js
+++ Module2/app.js
@@ -15,7 +15,32 @@ app.get('/about', (req, res) => {
 
 //Contact page
 app.get('/contact', (req, res) => {
-    res.sendFile("./contact.html", { root: __dirname});
+    res.send(`
+        <h1>Contact Us</h1>
+        <form method="GET" action="/submit">
+            <!-- Beginning of form -->
+            <label>Name:<input type="text" id="fullname" name="fullname" size="35" maxlength="50"></label>
+            <br><br>
+            <label>Email:<input type="text" id="email" name="email" size="50" maxlength="50"></label>
+            <br><br>
+            <label>Other form of contact:<input type="text" id="other" name="other" size="40" maxlength="50"></label>
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+    `);
+});
+
+//Submit
+app.get('/submit', (req, res) => {
+    const { fullname, email, other } = req.query;
+    res.send(`
+        <h1>Thank you, ${fullname}!</h1>
+        <p>We have received your message:</p>
+        <ul>
+            <li>Email: ${email}</li>
+            <li>Other contact: ${other}</li>
+        </ul>
+        `);
 });
 
 //Start our service
