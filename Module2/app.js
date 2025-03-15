4572c70019e1d437ff568e843c713235268587f5
ncasey-ppu <ncasey@pointpark.edu>
2025-03-14 22:22:22 -0400

    contact page
    attempted to set up contact page

--- Module2/app.js
+++ Module2/app.js
@@ -3,13 +3,35 @@ const app = express();
 
 const port = 3001;
 
-//Define a route
+//Homepage
 app.get('/', (req, res) => {
-    res.send('Welcome to My Express App!');
+    res.send('<h1>Welcome to My Express App!</h1>');
 });
 
+//About page
 app.get('/about', (req, res) => {
-    res.send('This is a simple Express application for learning purposes.');
+    res.send('<h1>About Us</h1><p>This is a simple Express application for learning purposes.</p>');
+});
+
+//Contact page
+app.get('/contact', (req, res) => {
+    res.send('
+        <h1>Contact Us</h1>
+        <form action="send-email.php" method="post">
+            Name:
+            <br>
+            <input type="text" name="full name" size="35" maxlength="40">
+            <br><br>
+            Email:
+            <br>
+            <input type="text" name="email" size="40" maxlength="50">
+            <br><br>
+            Other form of contact:
+            <br>
+            <input type="text" name="other" size="30" maxlength="50">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>');
 });
 
 //Start our service
