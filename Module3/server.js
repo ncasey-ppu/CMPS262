2b375d36971625bebf653c722372b17cdecd370e
ncasey-ppu <ncasey@pointpark.edu>
2025-03-20 12:21:52 -0400

    Update server.js
    started writing server file

---
+++ Module3/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = 8003;
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors({
+    origin: '*'
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello Point Park University");
+});
+
+app.listen(port, () => console.log('running on $(port)'));
