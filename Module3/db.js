9c0a5488410f8d64af33d9205a0905fd33bb3030
ncasey-ppu <ncasey@pointpark.edu>
2025-03-20 21:01:43 -0400

    correct errors
    correcting errors from failed test

--- Module3/db.js
+++ Module3/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "nikki",
-    host: "localhost",
+    host: "actuallocalhost",
     database: "classics",
     port: "5433",
 });
--- Module3/server.js
+++ Module3/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const moviesRoutes = require("./src/movies/routes");
 
 const app = express();
 const port = 8003;
@@ -13,5 +14,8 @@ app.use(cors({
 app.get("/", (req, res) => {
     res.send("Hello Point Park University");
 });
+
+//API Route
+app.use("/api/v1/movies", moviesRoutes);
 
 app.listen(port, () => console.log('running on $(port)'));
