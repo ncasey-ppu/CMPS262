644ddaeb38664c7add00ea6fd12b4451794a94f6
ncasey-ppu <ncasey@pointpark.edu>
2025-03-20 20:21:09 -0400

    Update db.js
    added code to db file

--- Module3/db.js
+++ Module3/db.js
@@ -1,7 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-
+    user: "nikki",
+    host: "localhost",
+    database: "classics",
+    port: "5433",
 });
 
 module.exports = pool;
