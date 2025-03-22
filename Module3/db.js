0155d8aba96a5505e806d5736d337a296dcb428e
ncasey-ppu <ncasey@pointpark.edu>
2025-03-22 09:21:39 -0400

    Update db.js
    added code for production server

--- Module3/db.js
+++ Module3/db.js
@@ -1,14 +1,18 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    //user: "nikki",
+    //dev
+    /*
+    user: "nikki",
+    host: "localhost",
+    database: "classicMovies",
+    port: "5433",*/
+
+    //Production
     user: "ncasey",
-    //host: "localhost",
     host: "dpg-cvf0i05umphs73epqre0-a",
-    //database: "classicMovies",
     database: "classicmovies",
     password: "B2uXGFQOYFhkmrCwlWtHSPbrCbT8bx2s",
-    //port: "5433",
     port: "5432",
 });
 
