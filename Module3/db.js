997ddd18238bbb36f7092fe298f57b6b06e5fc72
ncasey-ppu <ncasey@pointpark.edu>
2025-03-21 21:37:41 -0400

    setup remote database
    setup postman, render, and remote database in postgres

--- Module3/db.js
+++ Module3/db.js
@@ -1,10 +1,15 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "nikki",
-    host: "localhost",
-    database: "classicMovies",
-    port: "5433",
+    //user: "nikki",
+    user: "ncasey",
+    //host: "localhost",
+    host: "dpg-cvf0i05umphs73epqre0-a",
+    //database: "classicMovies",
+    database: "classicmovies",
+    password: "B2uXGFQOYFhkmrCwlWtHSPbrCbT8bx2s",
+    //port: "5433",
+    port: "5432",
 });
 
 module.exports = pool;
