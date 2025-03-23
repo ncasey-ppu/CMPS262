dfba890236d72372780c540134cf9074f13d2567
ncasey-ppu <ncasey@pointpark.edu>
2025-03-22 21:35:45 -0400

    Update db.js
    added production info after failed Render attempt

--- Module3/db.js
+++ Module3/db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    //dev
+    //local
+    /*
     user: "nikki",
     host: "localhost",
     database: "classicMovies",
-    port: "5433",
+    port: "5433", */
+
+    //Production
+    user: "ncasey",
+    host: "dpg-cvf0i05umphs73epqre0-a",
+    database: "classicmovies",
+    password: "B2uXGFQOYFhkmrCwlWtHSPbrCbT8bx2s",
+    port: "5432",
 });
 
 module.exports = pool;
