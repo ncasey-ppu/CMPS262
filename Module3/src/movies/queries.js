498ef821a45bbd68e5cd7b268127c325c5265081
ncasey-ppu <ncasey@pointpark.edu>
2025-03-25 21:27:16 -0400

    Update queries.js

--- Module3/src/movies/queries.js
+++ Module3/src/movies/queries.js
@@ -1,5 +1,5 @@
 const getMovies = "SELECT * FROM classics";
-const getMoviesById = "SELECT* FROM public.classics where "movieId" = $1";
+const getMoviesById = "SELECT* FROM public.classics where movieId = 4";
 
 module.exports = {
     getMovies,
