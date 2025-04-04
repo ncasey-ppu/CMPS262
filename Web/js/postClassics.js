59ec26e0fbc40de7724fab493a1c2d25b58ce92a
ncasey-ppu <ncasey@pointpark.edu>
2025-04-03 21:39:30 -0400

    added html, js and css to post a movie

---
+++ Web/js/postClassics.js
@@ -0,0 +1,23 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(formEl);
+    const data = Object.formEntries(formData);
+
+    if(data.title == "" || data.year == "" || data.time == ""){
+        $.toaster({priority : 'danger', title : 'Error', message : 'Incomplete data'});
+    }
+    else {
+        fetch('https://classicmovies.onrender.com/api/v1/movies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+          .then(data => console.log(data))
+          .then(error => console.log(error))
+          $.toaster({priority : 'success', title : 'Movie Add', message : 'New movie added to library'});
+    }
+})
